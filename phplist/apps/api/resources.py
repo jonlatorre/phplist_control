7938c2d8e815d527d593aba7f35478d48b60344c
Jon Latorre <moebius@etxea.net>
2013-07-19 19:47:35 +0200

    sincronizando con las bbdds del phplist

--- phplist/apps/api/resources.py
+++ phplist/apps/api/resources.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+
 from tastypie.resources import ModelResource, Resource
 from tastypie import fields
 from tastypie.authorization import DjangoAuthorization, Authorization
@@ -35,16 +53,21 @@ class ListaResource(ModelResource):
         #print ret
         return ret
     def hydrate(self, bundle):
-        print "Establecemos el sitio"
-        bundle.obj.sitio = Sitio.objects.get(nombre=bundle.data['sitio'])
-        print "Vamos con los usuarios"
+        nombre_sitio=bundle.data['sitio']
+        print "Establecemos el sitio con el nombre %s"%nombre_sitio
+        bundle.obj.sitio = Sitio.objects.get(nombre=nombre_sitio)
+        print "Vamos a procesar los usuarios"
         usuarios = bundle.data['usuarios']
+        #primero hacemos un save porque sino no nos deja hacer el usuarios.add
         bundle.obj.save()
         for nombre in usuarios:
             email = usuarios[nombre]
-            print nombre,email
-            u = Usuario(nombre=nombre,email=email)
-            u.save()
-            bundle.obj.usuarios.add(u)
+            usuario,created = Usuario.objects.get_or_create(nombre=nombre,email=email)
+            if created:
+                print "Creando el usuario %d %s-%s"%(usuario.id,usuario.nombre,usuario.email)
+            else:
+                print "Ya existia el user %d %s-%s"%(usuario.id,usuario.nombre,usuario.email)
+            print "Añadimos el user"
+            bundle.obj.usuarios.add(usuario)
         return bundle
         
--- phplist/apps/listas/models.py
+++ phplist/apps/listas/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings
+import MySQLdb as mdb
+import hashlib, sys
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=25)
@@ -10,7 +13,7 @@ class Usuario(models.Model):
     
 class Sitio(models.Model):
     dominio = models.CharField(max_length=100)
-    nombre = models.CharField(max_length=25)
+    nombre = models.CharField("el nombre del sitio, se usa para saber el nombre de la BBDD",max_length=25)
     def __unicode__(self):
         return "%s"%self.nombre
     def get_absolute_url(self):
@@ -25,3 +28,68 @@ class Lista(models.Model):
         return "%s"%self.nombre
     def get_absolute_url(self):
         return "/listas/%d/"%self.id
+	def save(self):
+		print "Somos la funcion personal de guardar"
+		if self.pk is None:
+			print "Es la primera vez que nos salvan.."
+		super(Lista, self).save()
+
+def sincronizar_sitios_listas():
+	for sitio in Sitio.objects.all():
+		print("Vamos a sincronizar las listas de %s"%sitio.nombre)
+		nombre_bbdd = "phplist_%s"%sitio.nombre
+		##El id de el usuario no inicializamos a 1 en cada sitio
+		user_id=1
+		try:
+			con = mdb.connect(settings.BBDD_SERVER, settings.BBDD_USER, settings.BBDD_PASSWD, nombre_bbdd)
+			print "\tConectado a la BBDD"
+			cur = con.cursor()
+			print "\tBorrando las listas"
+			sql = "TRUNCATE  phplist_list"
+			cur.execute(sql)
+			print "\tBorrando los usuarios"
+			sql = "TRUNCATE  phplist_user_user"
+			cur.execute(sql)
+			print "\tBorrando las asociaciones usurios listas"
+			sql = "TRUNCATE  phplist_listuser"
+			cur.execute(sql)
+			
+			for lista in sitio.lista_set.all():
+				if lista.nombre == "":
+					continue
+				print "\tVamos a trabajar con la lista...%s"%lista.nombre
+				listorder="0"
+				prefix="NULL"
+				rssfeed="NULL"
+				active="1"
+				owner="1"
+				sql = "INSERT INTO phplist_list VALUES("+str(lista.id)+",'"+lista.nombre+"','"+lista.nombre+"',NOW() ,"+listorder+","+prefix+","+rssfeed+",NOW(),"+active+","+owner+")"
+				cur.execute(sql)
+
+				for usuario in lista.usuarios.all():
+					print "\tVamos a anadir el usuario %s"%usuario.nombre
+					sql="SELECT id FROM phplist_user_user WHERE email='"+str(usuario.email)+"'"
+					cur.execute(sql)
+					res = cur.fetchall()
+					if len(res) > 0:
+						print "\t\tYa existe el user %s"%res[0]
+						temp_user_id="%s"%res[0]
+						sql="INSERT INTO phplist_listuser VALUES("+temp_user_id+","+str(lista.id)+",NOW(),NOW())"
+						#print sql
+						cur.execute(sql)
+					else:
+						print "\t\tCreamos user nuevo"
+						h = hashlib.sha1(usuario.email+settings.SECRET_KEY)
+						h = h.hexdigest()
+						sql="INSERT INTO phplist_user_user VALUES("+str(user_id)+",'"+usuario.email+"',1,0,0,NOW(),NOW(),'"+str(h)+"',1,NULL,NULL,NULL,NULL,0,NULL,NULL)"
+						cur.execute(sql)
+						user_id=user_id+1
+						sql="INSERT INTO phplist_listuser VALUES("+str(user_id)+","+str(lista.id)+",NOW(),NOW())"
+						#print sql
+						cur.execute(sql)
+					
+			print "Hemos terminado con el sitio %s"%sitio.nombre
+			con.close()
+		except mdb.Error, e:
+			print "Error %d: %s" % (e.args[0], e.args[1])
+			sys.exit(1)
